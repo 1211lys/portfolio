5b0ecc5124208452d20d20312c1afd23d66ceab4
1211lys <hpppower111@gmail.com>
2022-08-07 19:35:18 +0900

    scroll update

--- event.js
+++ event.js
@@ -16,3 +16,26 @@ document.addEventListener('scroll', () => {
     console.log('false');
   }
 });
+
+const $top = document.querySelector(".moveTop");
+
+$top.onclick = () => {
+  window.scrollTo(0, 0);  
+}
+
+const $bottom = document.querySelector(".moveBottom");
+
+$bottom.onclick = () => {
+  window.scrollTo(0, document.body.scrollHeight);  
+}
+// const navbar = document.querySelector('#navbar');
+// const navbarHeight = navbar.getBoundingClientRect().height;
+// document.addEventListener('scroll', () => {
+//   if (window.scrollY > navbarHeight) {
+//     navbar.classList.add('nav_scroll');
+//     console.log('if')
+//   } else {
+//     navbar.classList.remove('nav_scroll');
+//     console.log('else')
+//   }
+// });
